00963b3324c866fc74678af02b490e8d0f86c195
Chris Corbett <w0737064@myscc.ca>
2021-12-08 14:51:58 -0500

    Fixed second container

---
+++ main.js
@@ -0,0 +1,26 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: "Hello, world!"
+    }
+})
+
+var app2 = new Vue({
+    el: '#app2',
+    data: {
+        message1: "Message 1",
+        message2: "Message 2",
+        message3: "Message 3"
+    }
+})
+
+var app3 = new Vue({
+    el: '#app3',
+    data: {
+        todos: [
+            { text: "Todo 1" },
+            { text: "Here this text is different" },
+            { text: "Todo 3" }
+        ]
+    }
+})
