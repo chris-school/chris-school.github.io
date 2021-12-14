62c36e3bec05cfa3d2e1acedffd7ceaa81fdaf30
chris-school <w0737064@myscc.ca>
2021-12-13 19:56:14 -0500

    Mostly done just need to change description for Portfolio Website project

--- main.js
+++ main.js
@@ -1,54 +1,73 @@
-const NotFound = { template: '<p>Page not found</p>' }
-const Home = { template: '<p>Home page</p>' }
-const Detail = { template: '<p>Detail page</p>' }
+Vue.component('portfolio-project', {
+    props: ['project'],
+    template: `
+        <div class="item">
+            <h3>{{ project.title }}</h3>
 
-const routes = {
-    '/': Home,
-    '/details': Detail
-}
+            <img v-if="project.image" v-bind:src="project.image" alt="Project image">
 
-new Vue({
-    el: '#app',
-    data: {
-        currentRoute: window.location.pathname
-    },
-    computed: {
-        ViewComponent() {
-            return routes[this.currentRoute] || NotFound
+            <p v-if="!seen">{{project.description}}</p>
+
+            <hr>
+
+            <div class="d-grid gap-2 col-6 mx-auto info-btn">
+                <button v-on:click="toggleInfo" class="btn btn-dark" type="button">{{this.button.text}}</button>
+            </div>
+        </div>
+    `,
+    methods: {
+        toggleInfo: function() {
+            this.seen = !this.seen
+            this.button.text = this.seen ? 'Show more' : 'Show less'
         }
     },
-    render (h) { return h(this.ViewComponent)}
-})
+    data() {
+        return {
+            seen: true,
+            button: {
+                text: 'Show more'
+            }
+        }
+    }
+}) 
+
+{/* <button v-on:click="toggleInfo" class="btn btn-dark" type="button">{{ button.text }}</button> */}
 
 var app = new Vue({
-    el: '#app2',
+    el: '#app',
     data: {
         projects: [
             { 
+                id: 0,
                 title: "Portfolio Website",
                 image: 'images/portfolio_screenshot_3.png',
-                description: "Here is a description"
+                description: "Here is a different description."
             },
             { 
+                id: 1,
                 title: "Placeholder",
                 description: "Here is a description"
             },
             { 
+                id: 2,
                 title: "Placeholder",
                 description: "Here is a description"
             },
             { 
+                id: 3,
                 title: "Placeholder",
                 description: "Here is a description"
             },
             { 
+                id: 4,
                 title: "Placeholder",
                 description: "Here is a description"
             },
             { 
+                id: 5,
                 title: "Placeholder",
                 description: "Here is a description"
             }
-        ]
+        ],
     }
 })
